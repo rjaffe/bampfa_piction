05588a7a0cec399948d9d14e72451047a8fbc97f
Rick Jaffe <rjaffe@berkeley.edu>
2016-02-14 17:25:25 -0800

    Changed this script to Python3

--- Python3_scripts/filter_all-metadata_REUPS_py3.py
+++ Python3_scripts/filter_all-metadata_REUPS_py3.py
@@ -8,24 +8,9 @@ import csv, sys, codecs
 # Key = everything in col 1 that follows "Company Home/.../Media/"
 # Value = entire row.
 # Then, read working-files_fileList.txt
-# For each line, if txt == a key in the dictionary, copy the value of that dictionary item to a match list.
+# For each line, if txt == a key in the dictionary, copy the value of \
+# that dictionary item to a match list.
 # When done, print the match list to a file.
-
-# Two functions for handling unicode input. From Python documentation at:
-# https://docs.python.org/2/library/csv.html#examples
-
-
-# def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
-#     # csv.py doesn't do Unicode; encode temporarily as UTF-8:
-#     csv_reader = csv.reader(utf_8_encoder(unicode_csv_data),
-#                             dialect=dialect, **kwargs)
-#     for row in csv_reader:
-#         # decode UTF-8 back to Unicode, cell by cell:
-#         yield [unicode(cell, 'utf-8') for cell in row]
-#
-# def utf_8_encoder(unicode_csv_data):
-#     for line in unicode_csv_data:
-#         yield line.encode('utf-8')
 
 # Replace sys.argv[1] with 'all-metadata_REUPS.csv' when running in pyCharm
 def cleanKeys(dict):
@@ -65,9 +50,12 @@ def makeDictFromCsv(f):
 def filterMetadataUsingFiles(fileList):
     """
     Takes text file with list of file names.
-    Filters .csv file to return only the rows in the .csv file that correspond to the files in the list.
+    Filters .csv file to return only the rows in the .csv file that correspond to
+    the files in the list.
 
-    Prints new .csv file.
+
+
+    Prints new .csv file, plus a .txt file listing files not included in .csv
     :param fileList:
     :return:
     """
@@ -75,22 +63,30 @@ def filterMetadataUsingFiles(fileList):
     outputcsvfile = open('filtered_' + 'all-metadata_REUPS_py3.csv', 'w', newline='')
     outputwriter = csv.writer(outputcsvfile, delimiter=",", quoting=csv.QUOTE_ALL)
 
+    # Try this instead:
+    # global outputwriter
+    #    # Replace sys.argv[1] with 'all-metadata_REUPS.csv' when running in pyCharm
+    #    with open('filtered_' + 'all-metadata_REUPS_py3.csv', 'w', newline='') as outputcsvfile:
+    #        outputwriter = csv.writer(outputcsvfile, delimiter=",", quoting=csv.QUOTE_ALL)
+
+
     mycleandict = {}
-#    myFilteredList = []
 
     # Replace sys.argv[1] with 'all-metadata_REUPS.csv' when running in pyCharm
     mycleandict = makeDictFromCsv('../all-metadata_REUPS.csv')
 
     with codecs.open(fileList, 'r', 'utf-8' ) as f:
-        myfiles = [line.rstrip('\n') for line in f]
-        for myfile in myfiles:
-            if myfile in mycleandict:
-#                myFilteredList.append(mycleandict[myfile])
-                row = mycleandict[myfile]
-                outputwriter.writerow(row)
+        with open('notInCSV.txt', 'w') as f_not:
+            myfiles = [line.rstrip('\n') for line in f]
+            for myfile in myfiles:
+                if myfile in mycleandict:
+                    row = mycleandict[myfile]
+                    outputwriter.writerow(row)
+                else:
+                    notInCsv = str(myfile + '\n')
+                    f_not.write(notInCsv)
 
-#    DEBUG = "Uncomment this line to pause here before finishing"
 
-# TODO: Refactor to allow user to pass in files list as argument.
+# TODO: Refactor to allow user to pass in files list as argument (sysargv[2]?).
 filterMetadataUsingFiles('../working-files-file-list.txt')
-#filterMetadataUsingFiles('reupthese-files_fileList.txt')
+
